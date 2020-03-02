9e318945fe7d306846d1dcbe6205a8959ae81f2b
priyamvada <pranshulpandey5@gmail.com>
2020-03-02 01:51:48 +0000

    completed

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production' ){
-	module.exports = require('./prod-config');
+	module.exports = require('./prod-config')
 }
 else{
 	module.exports = require('./dev-config');
